b1ae7385e8b4cbde5ea4c589359af8b913d10d68
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-17 19:12:26 -0700

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -9,8 +9,12 @@ test('renders the AnimalForm component without errors', () => {
 test('when a user fills out and submits the form, a new animal is created and displayed', () => {
     // Arrange - render the component and get access to the form elements in the DOM
     render(<AnimalForm />);
+    const speciesInput = screen.getByLabelText(/species/i);
+    const speciesInput = screen.getByLabelText(/species/i);
+    const speciesInput = screen.getByLabelText(/species/i);
 
     // Act - type into the form, filling out all fields of the form, then submit
+
 
     // Assert - make sure that the new animal we created is displayed in the list
 
