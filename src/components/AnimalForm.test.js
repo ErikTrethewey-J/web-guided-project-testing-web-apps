9842a5227460d4b6c751890e2b3370c497de6916
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-17 19:21:41 -0700

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -13,11 +13,13 @@ test('when a user fills out and submits the form, a new animal is created and di
     const speciesInput = screen.getByLabelText(/species/i);
     const ageInput = screen.getByLabelText(/age/i);
     const notesInput = screen.getByLabelText(/notes/i);
+    const submitButton = screen.getByRole("button", { name: /subasldkfjmit/i });
 
     // Act - type into the form, filling out all fields of the form, then submit
     userEvent.type(speciesInput, "Grizzly Bear");
     userEvent.type(ageInput, "97");
     userEvent.type(notesInput, "very cuddly and friendly, but don't threaten children or bring peanut butter nearby.");
+    userEvent.click(submitButton);
 
     // Assert - make sure that the new animal we created is displayed in the list
 
