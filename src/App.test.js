d47a41113688bd97655f61727c6c51ba1fa254ec
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-17 18:28:43 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+
+// basic test format: test('description of test', () => {
+    // Arrange: render the component and find the elements you need to work with
+    // Act: do stuff -- fill out forms, click buttons, etc
+    // Assert: make sure that the component behaved in the way you'd expect it to.
+// })
+
+test('renders App without errors', () => {
+    // Arrange
+    render(<App />);
+})
+
